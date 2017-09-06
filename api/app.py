45d806d7e3e447644446c1786818002112b30f34
leotok <leo.wajnsztok@gmail.com>
2017-09-06 01:08:55 -0300

    API ok

---
+++ api/app.py
@@ -0,0 +1,82 @@
+import json
+import os, sys
+
+from flask import Flask, render_template, redirect, url_for, request, send_from_directory
+from werkzeug.utils import secure_filename
+
+REPO_PATH = os.path.dirname(os.path.abspath(os.path.dirname((__file__))))
+ML_PATH = os.path.join(REPO_PATH, "ml")
+print ML_PATH
+
+sys.path.append(REPO_PATH)
+sys.path.append(ML_PATH)
+
+from ml.predict import predict_pokemon
+
+
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+FULL_UPLOAD_PATH = os.path.abspath(os.path.dirname(__file__)) + app.config['UPLOAD_FOLDER']
+
+############## Helper functions ##############
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+############## Views ##############
+
+@app.route('/')
+def index():
+    try:
+        pokemon_image = os.path.join(UPLOAD_FOLDER, request.args['pokemon_image'])
+    except:
+        pokemon_image = None
+
+    try:
+        pokemon_name = predict_pokemon(pokemon_image)
+    except:
+        pokemon_name = None
+    return render_template('index.html', pokemon_image=pokemon_image, pokemon_name=pokemon_name)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(FULL_UPLOAD_PATH,
+                               filename)
+
+@app.route('/predict/pokemon', methods=['POST'])
+def get_pokemon_info():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if os.path.isfile(file.filename):
+        path = os.path.join(FULL_UPLOAD_PATH, filename)
+        return redirect(url_for('index', pokemon_image=path))
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        path = os.path.join(FULL_UPLOAD_PATH, filename)
+        file.save(path)
+        return redirect(url_for('index', pokemon_image=filename))
+
+    return render_template('bad request')
+
+
+@app.route('/stop')
+def stop():
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
--- ml/predict.py
+++ ml/predict.py
@@ -8,13 +8,16 @@ import imutils
 import pickle
 import numpy as np
 
+FULL_UPLOAD_PATH = os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), "api")
+MODEL_PATH = os.path.dirname(os.path.abspath(__file__))
 
 def predict_pokemon(imagePath):
-    image = cv2.imread(imagePath)
+    fullpath = FULL_UPLOAD_PATH + imagePath
+    image = cv2.imread(fullpath)
     if image is not None:
         features = np.array([extract_color_histogram(image)])
-        loaded_model = pickle.load(open("pokemon_model.sav", 'rb'))
+        loaded_model = pickle.load(open(MODEL_PATH + "/pokemon_model.sav", 'rb'))
 
         return loaded_model.predict(features)[0]
     else:
-        return "Faile to load image"
+        raise "Failed"
